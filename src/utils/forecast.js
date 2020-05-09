4608056325e37b535100020629fdf47b5ec070bb
Quint van Zijl <quint.van.zijl@ah.nl>
2020-05-09 23:48:49 +0200

    removed console.log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,6 @@ const forecast = (latitude, longitude) => {
     if(!latitude || !longitude) return 'forecast.js - invalid params'
     const url = `http://api.weatherstack.com/current?access_key=6edabfb63087324fba55bfee385ad43b&query=${latitude},${longitude}`
     return axios(url).then((res) => {
-        console.log(res)
         const {temperature, feelslike, weather_descriptions} = res.data.current;
         return res.data.error ?
             'Unable to find location. Try another search query.' :
